21bfcb50bef1aafb93209fb797d40dfb81729664
majkon8 <48221693+majkon8@users.noreply.github.com>
2020-01-11 14:22:58 +0100

    Updates

---
+++ precache-manifest.d1d749e01917054fb42c122ade14d561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49dc27676639fddf43487ecafaec38d5",
+    "url": "/react-gitbattles/index.html"
+  },
+  {
+    "revision": "5b89a7ec6af84a985f3f",
+    "url": "/react-gitbattles/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "f70ebe8297c428543ce2",
+    "url": "/react-gitbattles/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5b89a7ec6af84a985f3f",
+    "url": "/react-gitbattles/static/js/2.14e84d14.chunk.js"
+  },
+  {
+    "revision": "f70ebe8297c428543ce2",
+    "url": "/react-gitbattles/static/js/main.5aca1ebe.chunk.js"
+  },
+  {
+    "revision": "3c8989ef9781898bcbcc",
+    "url": "/react-gitbattles/static/js/runtime~main.c59f72d9.js"
+  }
+]);
