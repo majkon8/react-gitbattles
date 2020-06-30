4621b534df48afb88e76047394961d67b68a649e
majkon8 <48221693+majkon8@users.noreply.github.com>
2020-06-30 21:31:52 +0200

    Add redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Redirect } from "react-router";
 import styled from "styled-components";
 import Nav from "./components/Nav/Nav";
 import Home from "./components/Home/Home";
